a7c1b45e5a3d825395095dfe6aa8bfef585fc16e
YoucefMebAmaneEllahGherbal <gherbalyoucef@gmail.com>
2026-08-04 19:11:14 +0100

    Refactor login screen layout and enhance text field styling

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -6,89 +6,100 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kBackgroundColor,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "GymPulse",
-            style: TextStyle(
-              color: kLightAccentColor,
-              fontWeight: FontWeight.bold,
-              fontSize: 35,
-            ),
-          ),
-          Text(
-            "Welcome Back",
-            style: TextStyle(
-              color: kLightAccentColor,
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: TextField(
-              keyboardType: TextInputType.emailAddress,
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                hintText: "Enter Your Email",
-                filled: true,
-                fillColor: kLightAccentColor,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: TextField(
-              obscureText: true,
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                hintText: "Enter Your Password",
-                filled: true,
-                fillColor: kLightAccentColor,
-              ),
-            ),
-          ),
-          SizedBox(height: 25),
-          SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: WidgetStatePropertyAll(kAccentColor),
-              ),
-              onPressed: () {},
-              child: Text(
-                "Login",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-          ),
-          TextButton(
-            onPressed: () {
-              print("forgot password button pressed ");
-            },
-            child: Text(
-              "Forgot your password?",
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: kBackgroundColor,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "GymPulse",
               style: TextStyle(
                 color: kLightAccentColor,
-                fontSize: 12,
                 fontWeight: FontWeight.bold,
+                fontSize: 35,
               ),
             ),
-          ),
-        ],
+            Text(
+              "Welcome Back",
+              style: TextStyle(
+                color: kLightAccentColor,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20.0,
+                vertical: 8,
+              ),
+              child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  hintText: "Enter Your Email",
+                  filled: true,
+                  fillColor: kLightAccentColor,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20.0,
+                vertical: 8,
+              ),
+              child: TextField(
+                obscureText: true,
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  hintText: "Enter Your Password",
+                  filled: true,
+                  fillColor: kLightAccentColor,
+                ),
+              ),
+            ),
+            SizedBox(height: 15),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(kAccentColor),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                print("forgot password button pressed ");
+              },
+              child: Text(
+                "Forgot your password?",
+                style: TextStyle(
+                  color: kLightAccentColor,
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
