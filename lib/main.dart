2bc2abb943542da011aefd78e9ad1253a08f80c5
YoucefMebAmaneEllahGherbal <gherbalyoucef@gmail.com>
2026-08-04 20:39:31 +0100

    Set HomeScreen as the initial route in the app

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RegistrationScreen(),
+      home: HomeScreen(),
       
 
       routes: {
