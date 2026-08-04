ff5d7a43363a898325b34930a5e3e423c8525f2b
YoucefMebAmaneEllahGherbal <gherbalyoucef@gmail.com>
2026-08-04 19:40:41 +0100

    Enhance registration screen layout and styling with new text fields and button

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -8,8 +8,117 @@ class RegistrationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
+    return SafeArea(
+      child: Scaffold(
         backgroundColor: kBackgroundColor,
-    ));
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "GymPulse",
+              style: TextStyle(
+                color: kLightAccentColor,
+                fontWeight: FontWeight.bold,
+                fontSize: 35,
+              ),
+            ),
+            Text(
+              "Welcome Back",
+              style: TextStyle(
+                color: kLightAccentColor,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20.0,
+                vertical: 8,
+              ),
+              child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: "Enter Your E-mail",
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20.0,
+                vertical: 8,
+              ),
+              child: TextField(
+                obscureText: true,
+                textAlign: TextAlign.center,
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: "Enter Your Username",
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20.0,
+                vertical: 8,
+              ),
+              child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: "Enter Your Phone Number",
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20.0,
+                vertical: 8,
+              ),
+              child: TextField(
+                obscureText: true,
+                textAlign: TextAlign.center,
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: "Enter Your Password",
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20.0,
+                vertical: 8,
+              ),
+              child: TextField(
+                obscureText: true,
+                textAlign: TextAlign.center,
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: "Enter Your Password",
+                ),
+              ),
+            ),
+            SizedBox(height: 15),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(kAccentColor),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "Register",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
