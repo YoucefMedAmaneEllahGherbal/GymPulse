13c3bc0f29240806857a7c794884db1aa7862a75
YoucefMebAmaneEllahGherbal <gherbalyoucef@gmail.com>
2026-08-05 12:19:33 +0100

    Refactor LoginScreen layout for improved structure and user experience

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -46,93 +46,115 @@ class LoginScreen extends StatelessWidget {
                 fontSize: 20,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20.0,
-                vertical: 8,
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: kSecondaryColor,
               ),
-              child: TextField(
-                keyboardType: TextInputType.emailAddress,
-                textAlign: TextAlign.center,
-                decoration: kTextFieldDecoration.copyWith(
-                  hintText: "Enter Your E-mail",
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20.0,
-                vertical: 8,
-              ),
-              child: TextField(
-                obscureText: true,
-                textAlign: TextAlign.center,
-                decoration: kTextFieldDecoration.copyWith(
-                  hintText: "Enter Your Password",
-                ),
-              ),
-            ),
-            SizedBox(height: 15),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: WidgetStatePropertyAll(kAccentColor),
-                  ),
-                  onPressed: () {},
-                  child: Text(
-                    "Login",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
+              height: 270,
+              width: 350,
+
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20.0,
+                      vertical: 8,
+                    ),
+                    child: TextField(
+                      keyboardType: TextInputType.emailAddress,
+                      textAlign: TextAlign.center,
+                      decoration: kTextFieldDecoration.copyWith(
+                        hintText: "Enter Your E-mail",
+                      ),
                     ),
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20.0,
+                      vertical: 8,
+                    ),
+                    child: TextField(
+                      obscureText: true,
+                      textAlign: TextAlign.center,
+                      decoration: kTextFieldDecoration.copyWith(
+                        hintText: "Enter Your Password",
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStatePropertyAll(kAccentColor),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      print("forgot password button pressed ");
+                    },
+                    child: Text(
+                      "Forgot your password?",
+                      style: TextStyle(
+                        color: kLightAccentColor,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            TextButton(
-              onPressed: () {
-                print("forgot password button pressed ");
-              },
-              child: Text(
-                "Forgot your password?",
-                style: TextStyle(
-                  color: kLightAccentColor,
-                  fontSize: 12,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
+
             Text(
-              "or login with : ",
+              "Or login with : ",
               style: TextStyle(
                 color: kLightAccentColor,
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: FaIcon(
-                    FontAwesomeIcons.googlePlus,
-                    color: Colors.white,
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: kSecondaryColor,
+              ),
+              width: 170,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: FaIcon(
+                      FontAwesomeIcons.googlePlus,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: FaIcon(FontAwesomeIcons.facebook, color: Colors.blue),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: FaIcon(FontAwesomeIcons.github, color: Colors.white),
-                ),
-              ],
+                  IconButton(
+                    onPressed: () {},
+                    icon: FaIcon(FontAwesomeIcons.facebook, color: Colors.blue),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: FaIcon(FontAwesomeIcons.github, color: Colors.white),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
