4831806afe411a1dae6d7d4ff09466cde6f11ece
YoucefMebAmaneEllahGherbal <gherbalyoucef@gmail.com>
2026-08-08 14:57:15 +0100

    Refactor AccountPage to StatefulWidget; implement user data retrieval and display email and phone number

--- lib/screens/home/account_page.dart
+++ lib/screens/home/account_page.dart
@@ -2,8 +2,38 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../../constants.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class AccountPage extends StatelessWidget {
+class AccountPage extends StatefulWidget {
+  @override
+  State<AccountPage> createState() => _AccountPageState();
+}
+
+class _AccountPageState extends State<AccountPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getUserData();
+  }
+
+  String? email = '';
+  String? userName = '';
+  String? phoneNumber = '';
+  void getUserData() async {
+    final userId = FirebaseAuth.instance.currentUser!.uid;
+    final userDoc = FirebaseFirestore.instance.collection('users').doc(userId);
+
+    final userData = await userDoc.get();
+
+    setState(() {
+      email = userData['email'];
+      userName = userData['username'];
+      phoneNumber = userData['phoneNumber'];
+    });
+  }
+
+  // final phoneNbr = FirebaseFirestore.instance
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -15,7 +45,17 @@ class AccountPage extends StatelessWidget {
           FaIcon(FontAwesomeIcons.userCircle, size: 64, color: kAccentColor),
           SizedBox(height: 12),
           Text(
-            'Account',
+            'Welcome $userName',
+            style: TextStyle(color: kLightAccentColor, fontSize: 18),
+          ),
+          SizedBox(height: 18),
+          Text(
+            'Your Email : $email',
+            style: TextStyle(color: kLightAccentColor, fontSize: 18),
+          ),
+          SizedBox(height: 18),
+          Text(
+            'Phone Number : $phoneNumber',
             style: TextStyle(color: kLightAccentColor, fontSize: 18),
           ),
           SizedBox(height: 18),
