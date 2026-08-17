7a3eafe6ed4b8b7560109a3f6998f77fd5b1e676
YoucefMebAmaneEllahGherbal <gherbalyoucef@gmail.com>
2026-08-17 13:08:39 +0100

    Enhance Admin Dashboard and Members Screen UI; implement navigation and data display for members

--- lib/screens/admin/admin_dashboard.dart
+++ lib/screens/admin/admin_dashboard.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gympulse_app/constants.dart';
+import 'admin_members_screen.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AdminDashboard extends StatelessWidget {
   const AdminDashboard({super.key});
@@ -6,13 +9,45 @@ class AdminDashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: kBackgroundColor,
       appBar: AppBar(
-        title: const Text('GymPulse Admin'),
+        centerTitle: true,
+        backgroundColor: kBackgroundColor,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.admin_panel_settings,
+              size: 32,
+              color: kLightAccentColor,
+            ),
+            SizedBox(width: 6),
+            Text(
+              'GymPulse Admin',
+              style: TextStyle(
+                color: kLightAccentColor,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(1),
+          child: Container(color: kAccentColor, height: 1),
+        ),
       ),
-      body: const Center(
-        child: Text(
-          'Welcome Admin',
-          style: TextStyle(fontSize: 25),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const AdminMembersScreen(),
+              ),
+            );
+          },
+          child: const Text('Members'),
         ),
       ),
     );
--- lib/screens/admin/admin_members_screen.dart
+++ lib/screens/admin/admin_members_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import '../../constants.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AdminMembersScreen extends StatelessWidget {
   const AdminMembersScreen({super.key});
@@ -6,14 +9,58 @@ class AdminMembersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: kBackgroundColor,
       appBar: AppBar(
-        title: const Text('Members'),
-      ),
-      body: const Center(
-        child: Text(
-          'Gym Members',
-          style: TextStyle(fontSize: 25),
+        iconTheme: IconThemeData(color: kLightAccentColor),
+        centerTitle: true,
+        backgroundColor: kBackgroundColor,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.people_alt, color: kAccentColor, size: 32),
+            SizedBox(width: 6),
+            Text(
+              'Members',
+              style: TextStyle(
+                color: kLightAccentColor,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
+
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(1),
+          child: Container(color: kAccentColor, height: 1),
+        ),
+      ),
+      body: FutureBuilder(
+        future: FirebaseFirestore.instance.collection('users').get(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          final users = snapshot.data!.docs;
+          return ListView.builder(
+            itemCount: users.length,
+            itemBuilder: (context, index) {
+              final user = users[index];
+
+              return ListTile(
+                title: Text(
+                  user['username'],
+                  style: TextStyle(color: kAccentColor, fontSize: 15),
+                ),
+                subtitle: Text(
+                  user['email'],
+                  style: TextStyle(color: kAccentColor, fontSize: 15),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
